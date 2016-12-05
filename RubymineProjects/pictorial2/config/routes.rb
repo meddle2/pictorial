c54b9c450a8889029657c877f486a3b01cd6d160
meddle <meddle777@gmail.com>
2016-12-05 18:43:17 +0100

    users accounts added

--- RubymineProjects/pictorial2/config/routes.rb
+++ RubymineProjects/pictorial2/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
 
+  get 'home/about' # About me site
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
