d69b4a787d177b3280167c9babd1b9a1371fc40a
meddle <meddle777@gmail.com>
2016-12-10 17:15:56 +0100

    new appearance, friendly for mobile devices

--- RubymineProjects/pictorial2/db/schema.rb
+++ RubymineProjects/pictorial2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208193557) do
+ActiveRecord::Schema.define(version: 20161208195042) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
